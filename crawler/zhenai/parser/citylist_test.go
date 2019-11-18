f1c79687d2872e177b032e0fe333b182e0157c89
letwant <letwant@github.com>
2019-11-18 22:37:15 +0800

    url解析测试完成

--- crawler/zhenai/parser/citylist_test.go
+++ crawler/zhenai/parser/citylist_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestParseCityList(t *testing.T) {
-	contents, err := ioutil.ReadFile("citylist_test_data.html")
+	contents, err := ioutil.ReadFile("citylist_test_data.dat")
 	if err != nil {
 		panic(err)
 	}
@@ -14,11 +14,23 @@ func TestParseCityList(t *testing.T) {
 	result := ParseCityList(contents)
 
 	const resultSize = 470
+	expectedUrls := []string{"http://www.zhenai.com/zhenghun/aba", "http://www.zhenai.com/zhenghun/akesu",
+		"http://www.zhenai.com/zhenghun/alashanmeng",}
+	expectedCities := []string{"阿坝", "阿克苏", "阿拉善盟",}
 	if len(result.Requests) != resultSize {
 		t.Errorf("result should have %d requests; but had %d",
 			resultSize, len(result.Requests))
 	}
-
+	for i, url := range expectedUrls {
+		if result.Requests[i].Url != url {
+			t.Errorf("expected url #%d: %s; but was %s", i, url, result.Requests[i].Url)
+		}
+	}
+	for i, city := range expectedCities {
+		if result.Items[i].(string) != city {
+			t.Errorf("expected city #%d: %s; but was %s", i, city, result.Items[i].(string))
+		}
+	}
 	if len(result.Items) != resultSize {
 		t.Errorf("result should have %d Items; but had %d",
 			resultSize, len(result.Items))
