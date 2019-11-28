4c2bd728fb708f2c34c71c6806b1183a9522e11d
DESKTOP-Q1QV6F5\Origin1119 <xiafei@originqc.com>
2019-11-28 14:28:21 +0800

    第十五章并发版爬虫完成

--- crawler/main.go
+++ crawler/main.go
@@ -11,8 +11,12 @@ func main() {
 		Scheduler:   &scheduler.QueuedScheduler{},
 		WorkerCount: 100,
 	}
+	//e.Run(engine.Request{
+	//	Url:        "http://www.zhenai.com/zhenghun",
+	//	ParserFunc: parser.ParseCityList,
+	//})
 	e.Run(engine.Request{
-		Url:        "http://www.zhenai.com/zhenghun",
-		ParserFunc: parser.ParseCityList,
+		Url:        "http://www.zhenai.com/zhenghun/shanghai",
+		ParserFunc: parser.ParseCity,
 	})
 }
--- crawler/zhenai/parser/city.go
+++ crawler/zhenai/parser/city.go
@@ -6,21 +6,32 @@ import (
 	"strings"
 )
 
-const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
+var (
+	profileRe = regexp.MustCompile(
+		`<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`)
+	cityUrlRe = regexp.MustCompile(
+		`<a href="(http://www.zhenai.com/zhenghun/[^"]+)`)
+)
 
 func ParseCity(contents []byte) engine.ParseResult {
-	re := regexp.MustCompile(cityRe)
-	matches := re.FindAllSubmatch(contents, -1)
+	matches := profileRe.FindAllSubmatch(contents, -1)
 	result := engine.ParseResult{}
 	for _, m := range matches {
 		name := string(m[2])
-		result.Items = append(result.Items, "User: "+ name)
+		result.Items = append(result.Items, "User: "+name)
 		url := strings.Replace(string(m[1]), "album", "m", -1)
 		result.Requests = append(result.Requests, engine.Request{
 			Url: url,
 			ParserFunc: func(c []byte) engine.ParseResult {
 				return ParseProfile(c, name)
 			},
+		})
+	}
+	matches = cityUrlRe.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		result.Requests = append(result.Requests, engine.Request{
+			Url:        string(m[1]),
+			ParserFunc: ParseCity,
 		})
 	}
 	return result
